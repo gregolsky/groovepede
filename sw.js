c9e509d65a2a27859429cb0b258964dd7208953f
Grzegorz Lachowski <gregory.lachowski@gmail.com>
2026-04-12 07:07:34 +0200

    fix service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,25 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/groovepede/sw.js', { scope: '/groovepede/' })
-    .then(() => console.log("Service Worker Registered"));
-}
+const CACHE = 'groovepede-v1';
+const ASSETS = [
+  '/groovepede/',
+  '/groovepede/index.html',
+  '/groovepede/manifest.json',
+  '/groovepede/favicon.png',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
